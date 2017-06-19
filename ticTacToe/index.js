b7bc626b1da17087492f26c470b8fd3dccc93691
Max Braz <braz.max@gmail.com>
2017-06-19 15:38:28 -0700

    working.  no check for a tie

--- ticTacToe/index.js
+++ ticTacToe/index.js
@@ -43,50 +43,48 @@ const validateMove = (position) => {
   return false;
 };
 
-// Everyone possible combination of three in a row
 var winCombinations = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7],
                        [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]];
 
-// Determins if the passed in player has three in a row
-function checkWin(player) {
+const checkWin = (player) => {
   for (var i = 0; i < winCombinations.length; i++) {
     var markCount = 0;
     for (var j = 0; j < winCombinations[i].length; j++) {
-        if (board[winCombinations[i][j]] === player) {
-            markCount++;
-          }
-        if (markCount === 3) {
-            return true;
-          }
+      if (board[winCombinations[i][j]] === player) {
+        markCount++;
       }
+      if (markCount === 3) {
+        return true;
+      }
+    }
   }
   return false;
-}
+};
 
-function playTurn(player) {
+const playTurn = (player) => {
 
   console.log('Your turn player: ' + player);
   prompt.start();
-  prompt.get(['position'], function (err, result) {
+  prompt.get(['position'], (err, result) => {
 
     if (validateMove(result.position) === true) {
-        markBoard(result.position, player);
-        printBoard();
-        if (checkWin(player) === true) {
-            console.log('Winner Winner!');
-            return;
-          }
-        if (player === 'X') {
-            playTurn('O');
-          } else {
-            playTurn('X');
-          }
-      } else {
-        console.log('incorrect input please try again...');
-        playTurn(player);
+      markBoard(result.position, player);
+      printBoard();
+      if (checkWin(player) === true) {
+        console.log('Winner Winner Chicken Dinner!');
+        return;
       }
+      if (player === 'X') {
+        playTurn('O');
+      } else {
+        playTurn('X');
+      }
+    } else {
+      console.log('incorrect input please try again...');
+      playTurn(player);
+    }
   });
-}
+};
 
 console.log('Game started: \n' +
     ' 1 | 2 | 3 \n' +
